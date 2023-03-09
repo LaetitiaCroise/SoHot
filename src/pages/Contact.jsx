e0b05a84ce876802d44fc389b8cfbdd9c4813709
Laetitia <laetitia.croise24@gmail.com>
2023-03-09 19:30:49 +0100

    formulaire ok

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Sharp from '../assets/sharp.jpg';
 import '../styles/Contact.css';
 function Contact() {
+ const { handleSubmit, register, reset, formState : {error}} = useForm ()
+ function onSubmit (data) {
+  console.log (data);
+  reset();
+  alert ('messages envoyé !')
+ }
   return (
     <div className='contact'>
         <div className='leftSide' style={{backgroundImage: `url(${Sharp})`}}></div>
         <div className='rightSide'>
             <h1>Contact</h1>
-            <form id="contact-form" method='POST'>
+            <form onSubmit = {handleSubmit(onSubmit)} id="contact-form" method='POST'>
                 <label htmlFor="name"> Nom</label>
-                <input name="name" placeholder=''type="text"/>
+                <input name="name" placeholder=''type="text"{...register("name", {required : true})}/>
                 <label htmlFor="email"> Email</label>
-                <input name="email" placeholder=''type="email"/>
+                <input name="email" placeholder=''type="email" {...register("email",{required : true})}/>
                 <label htmlFor="message">Message</label>
-                <textarea rows="6" placeholder='' name="message" required>
+                <textarea rows="10 
+                " placeholder='' name="message" required {...register("message",{required : true})}>
              </textarea>
              <button type="submit"> Envoyez !</button>
                 
